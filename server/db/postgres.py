535b571cf11f9d582e7d9afe59953f5a9f92b404
Saatwik Vasishtha <saatwik.vasishtha@gmail.com>
2024-09-10 13:50:14 +0530

    vector db

---
+++ server/db/postgres.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv['POSTGRESQL_DB_URL']
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+
+async def get_sql_db():
+    async with SessionLocal() as session:
+        yield session
